6009f9d85821f4c5475502746ac1a5933de9303c
Vicki Hamm <vicki.cello@gmail.com>
2018-04-18 02:48:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ function deepestChild() {
   return document.querySelector('#grand-node div div div div');
 }
 
-function increaseRanBy(n) {
+      const rankedLists = document.querySelectorAll('.ranked-list');
+      const firstList = rankedLists[0];
+      const secondList = rankedLists[1];
+      
+function increaseRankBy(n) {
   
 }
