c71d09e2906a573b671234920581cf3321aabf4e
Vicki Hamm <vicki.cello@gmail.com>
2018-04-18 02:45:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 
-function increaseRankBy(n) {
-  
+function deepestChild() {
+  return document.querySelector('#nested .target');
+}
+
+      const rankedLists = document.querySelectorAll('.ranked-list');
+      const firstList = rankedLists[0];
+      const secondList = rankedLists[1];
+
+      let children = firstList.children;
+      let start = 1;
+      for (let i = 0, l = children.length; i < l; i++) {
+        expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3);
+      }
+
+      children = secondList.children;
+      start = 12;
+
+      for (let i = 0, l = children.length; i < l; i++) {
+        expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3);
 }
