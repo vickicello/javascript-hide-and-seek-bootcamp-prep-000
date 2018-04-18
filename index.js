a1cefee5741bc6e10c6fab93de4e483c9bdb9451
Vicki Hamm <vicki.cello@gmail.com>
2018-04-18 02:46:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,22 +7,5 @@ function nestedTarget() {
 }
 
 function deepestChild() {
-  return document.querySelector('#nested .target');
-}
-
-      const rankedLists = document.querySelectorAll('.ranked-list');
-      const firstList = rankedLists[0];
-      const secondList = rankedLists[1];
-
-      let children = firstList.children;
-      let start = 1;
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).toEqual(start + i + 3);
-      }
-
-      children = secondList.children;
-      start = 12;
-
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).toEqual(start - i + 3);
+  return document.querySelector('#grand-node div div div div');
 }
